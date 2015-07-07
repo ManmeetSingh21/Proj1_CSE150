ce773ceffdb0aa4fd6580dda0d7759dcc0f397cc
ManmeetSingh21 <manmeet21singh@yahoo.com>
2015-07-07 16:35:00 -0700

    Update PriorityScheduler.java

--- threads/PriorityScheduler.java
+++ threads/PriorityScheduler.java
@@ -161,7 +161,7 @@ public class PriorityScheduler extends Scheduler {
 			
             		}
 	//incomptype thread state -> kthread
-            ThreadState FT = pickNextThread(); //ThreadState FT = pickNextThread();
+            KThread FT = pickNextThread(); //ThreadState FT = pickNextThread();
             
             if (FT != null) { 
             	
@@ -181,12 +181,12 @@ public class PriorityScheduler extends Scheduler {
 	 *		return.
 	 */
 	protected ThreadState pickNextThread() {
-		ThreadState next= null; //ThreadState X = null
+		KThread next= null; //ThreadState X = null
 		
 		
             	for (Iterator<KThread> TState = waitQueue.iterator(); TState.hasNext();) {  //for each ThreadState TState in waitQueue
             	
-                ThreadState thread = TState.next();  
+                KThread thread = TState.next();  
                 
                 
                 int priority = getThreadState(thread).getEffectivePriority(); //priority of the next thread
